de22a9a2e22a55268aa82ba1ad16af0071aeaf90
tz1006 <a@omg.tf>
2018-10-19 03:03:15 +0800

    Update ftid.py

--- ftid.py
+++ ftid.py
@@ -11,12 +11,15 @@ import futuquant as ft
 q = ft.OpenQuoteContext(host='198.13.60.168', port=11111)
 
 # 沪深A股
-hs = q.get_plate_stock('SH.3000005')
+hs = q.get_plate_stock('SH.3000005')[1]
 
 q.close()
 
+hs['code'] = hs['code'].map(lambda x: x[3:])
+
+
 # 富途牛牛Code
-ftid = hs[1].set_index('code').drop(columns=['stock_owner','stock_name', 'lot_size', 'stock_child_type', 'stock_type', 'list_time']).T.to_dict('records')[0]
+ftid = hs.set_index('code').drop(columns=['stock_owner','stock_name', 'lot_size', 'stock_child_type', 'stock_type', 'list_time']).T.to_dict('records')[0]
 
 print('ftid %d' % len(ftld))
 
