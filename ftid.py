c3a7f78f75ff7863c88f8a896e97b0c731c1c585
tz1006 <a@omg.tf>
2018-10-19 02:47:17 +0800

    Create ftid.py

---
+++ ftid.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+# -*- coding: UTF-8 -*- 
+# filename: ftid.py
+# version: 0.0.1
+# description: ftid.py
+
+
+import futuquant as ft
+
+
+q = ft.OpenQuoteContext(host='198.13.60.168', port=11111)
+
+# 沪深A股
+hs = q.get_plate_stock('SH.3000005')
+
+q.close()
+
+# 富途牛牛Code
+ftid = hs[1].set_index('code').drop(columns=['stock_owner','stock_name', 'lot_size', 'stock_child_type', 'stock_type', 'list_time']).T.to_dict('records')[0]
+
+print('ftid %d' % len(ftld))
+
