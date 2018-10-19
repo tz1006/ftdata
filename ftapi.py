e13cd36b6b320684eb2ba04a7fd2b19022de268e
tz1006 <a@omg.tf>
2018-10-19 17:29:55 +0800

    Update ftapi.py

--- ftapi.py
+++ ftapi.py
@@ -7,4 +7,21 @@
 
 
 import requests
+import time
 
+from ftid import ftid
+
+
+# 参数
+
+
+
+s = requests.session()
+s.keep_alive = False
+
+
+def get_data():
+    url = 'http://hq.sinajs.cn/list=%s' % sscode(code)
+    r = None
+    while r == None:
+        r = s.get(url, timeout=timeout)
