14bb964d94999cfafeb121ca5cd4a755fd60d668
tz1006 <a@omg.tf>
2018-10-25 05:35:23 +0800

    Update ftapi.py

--- ftapi.py
+++ ftapi.py
@@ -12,6 +12,42 @@ import time
 from ftid import ftid
 
 
+import futuquant as ft
+
+q = ft.OpenQuoteContext(host='sz.omg.tf', port=11111)
+
+# 一次API
+
+def get_history_kline(ftcode):
+    data = q.get_history_kline(ftcode, start='2018-01-20', end='2019-06-22')[1]
+    return data
+
+def get_stock_basicinfo(ftcode):
+    market = ftcode[:2]
+    data = q.get_stock_basicinfo(market, 'STOCK', ftcode)[1]
+    return data
+
+
+
+# 二次API                       
+                        
+def closing_average(ftcode, day):
+    kline = q.get_history_kline(ftcode)[1]
+    average = kline[-day:]['close'].mean()
+    return average
+
+                        
+def name_listing_suspension(ftcode):
+    data = get_stock_basicinfo(ftcode)
+    name = data['name'].to_string(index=False)
+    listing_date = data['listing_date'].to_string(index=False)
+    suspension = data['suspension']
+    #stock_id = data['stock_id']
+    #delisting = data['delisting']
+    info = [name, listing_date, suspension]
+    return info
+
+
 # 参数
 
 
