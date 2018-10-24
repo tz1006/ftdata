1d11bdc55a33dca5f779b05798f049ad14b83a05
tz1006 <a@omg.tf>
2018-10-25 06:57:51 +0800

    Update data.py

--- data.py
+++ data.py
@@ -16,7 +16,7 @@ q = ft.OpenQuoteContext(host='sz.omg.tf', port=11111)
 # 沪深A股
 hs = q.get_plate_stock('SH.3000005')[1]
 
-hs['code'] = hs[1]['code'].map(lambda x: x[3:])
+#hs['code'] = hs[1]['code'].map(lambda x: x[3:])
 
 # 富途牛牛Code
 ftcode_dict = hs.set_index('code').drop(columns=['stock_owner','stock_name', 'lot_size', 'stock_child_type', 'stock_type', 'list_time']).T.to_dict('records')[0]
@@ -38,8 +38,7 @@ def init_df():
 #def update_df():
 
 def df_insert_ma(ftcode):
-    # code = ccode(ftcode)
-    print(ftcode)
+    # print(ftcode)
     nls = name_listing_suspension(ftcode)
     name = nls[0]
     listing = nls[1]
@@ -49,8 +48,9 @@ def df_insert_ma(ftcode):
         average_data = closing_average(ftcode)
         last_4_average = average_data[0]
         last_9_average = average_data[1]
-        df.loc[len(df)] = [ftcode, name, last_4_average, last_9_average]
+        last_19_average = average_data[2]
+        if last_9_average/1.03 < last_4_average < last_9_average < last_19_average:
+            df.loc[len(df)] = [ftcode, name, last_4_average, last_9_average]
         #print('添加(%s, %s, %s)' % (code, last_4_average,last_9_average))
 
-    
-    
+
