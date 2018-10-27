ed03bf5cc55247b6e5ec2b38d0f609acbfe94245
tz1006 <a@omg.tf>
2018-10-27 20:20:55 +0800

    Update ftid.py

--- ftid.py
+++ ftid.py
@@ -8,7 +8,7 @@
 import futuquant as ft
 
 
-q = ft.OpenQuoteContext(host='198.13.60.168', port=11111)
+q = ft.OpenQuoteContext(host='sz.omg.tf', port=11111)
 
 # 沪深A股
 hs = q.get_plate_stock('SH.3000005')[1]
